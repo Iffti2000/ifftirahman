41eb2218409247338a019e11bbc60d92324be71c
unknown <iftirahman335@gmail.com>
2025-07-10 01:09:28 +0600

    Created a functional contact form and completed my portfolio website

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Header from './components/Header'
 import About from './Components/About'
 import Contact from './Components/Contact'
+ import { ToastContainer } from 'react-toastify';
 
 const App = () => {
   return (
     <div>
+      <ToastContainer/>
       <Header/>
       <About/>
       <Contact/>
